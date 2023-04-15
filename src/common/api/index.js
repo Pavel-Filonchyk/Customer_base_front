7e9b894ad2f3c85a27f0f9c0b442cd4f7500fc09
Pavel Filonchyk <p_filonchyk@mail.ru>
2023-04-15 18:46:25 +0300

    исправил путь к серверу

--- src/common/api/index.js
+++ src/common/api/index.js
@@ -5,7 +5,7 @@
 //     REACT_APP_SERVER_URL
 // } = APP_CONFIG
 
-export const GET_CUSTOMERS = 'http://134.122.57.16/list'
-export const POST_CUSTOMER = 'http://134.122.57.16/postCustomer'
-export const DELETE_CUSTOMER = 'http://134.122.57.16/delete'
-export const EDIT_CUSTOMER = 'http://134.122.57.16/customer'
+export const GET_CUSTOMERS = 'http://134.122.57.16:3001/list'
+export const POST_CUSTOMER = 'http://134.122.57.16:3001/postCustomer'
+export const DELETE_CUSTOMER = 'http://134.122.57.16:3001/delete'
+export const EDIT_CUSTOMER = 'http://134.122.57.16:3001/customer'
